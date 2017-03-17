4e20aa1c33038f0f81f6c5a2c913a671d6f0da46
Felipe Campelo <fccampelo@users.noreply.github.com>
2017-03-17 01:17:10 -0300

    Update create.js

--- src/create.js
+++ src/create.js
@@ -5,34 +5,40 @@ const rimraf = require('rimraf')
 const shelljs = require('shelljs')
 const exists = require('./exists')
 
-function cache (source, pkg) {
-	return new Promise((resolve, reject) => {
-		try {
-			rimraf.sync(source)
-			exists.folder(source)
-			fs.writeFileSync(`${source}/package.json`, fs.readFileSync(pkg))
-			shelljs.exec(`cd ${source} && npm i --prod --ignore-script`)
-			resolve()
-		} catch (err) {
-			reject(err)
-		}
-	})
-}
+class create () {
 
-function files (source) {
-	source = source + '/node_modules'
-	const files = fs.readdirSync(source)
-		.map(data => path.resolve(source, data))
-		.filter(data => fs.statSync(data).isDirectory())
-		.filter(data => fs.statSync(`${data}/package.json`).isFile())
-		.filter(data => require(`${data}/package.json`).main)
-		.map(data => path.resolve(data, require(`${data}/package.json`).main))
-		.filter(data => fs.statSync(data).isFile())
-	return {
-		js: files.filter(data => path.extname(data) === '.js'),
-		css: files.filter(data => path.extname(data) === '.css')
+	constructor(){
+
+	  throw new Error ('This class can not be instantiated');    
+
+	}
+	 static cache(source, pkg) {
+		return new Promise((resolve, reject) => {
+			try {
+				rimraf.sync(source)
+				exists.folder(source)
+				fs.writeFileSync(`${source}/package.json`, fs.readFileSync(pkg))
+				shelljs.exec(`cd ${source} && npm i --prod --ignore-script`)
+				resolve()
+			} catch (err) {
+				reject(err)
+			}
+		})
+	}
+
+	static files(source) {
+		source = source + '/node_modules'
+		const files = fs.readdirSync(source)
+			.map(data => path.resolve(source, data))
+			.filter(data => fs.statSync(data).isDirectory())
+			.filter(data => fs.statSync(`${data}/package.json`).isFile())
+			.filter(data => require(`${data}/package.json`).main)
+			.map(data => path.resolve(data, require(`${data}/package.json`).main))
+			.filter(data => fs.statSync(data).isFile())
+		return {
+			js: files.filter(data => path.extname(data) === '.js'),
+			css: files.filter(data => path.extname(data) === '.css')
+		}
 	}
 }
-
-exports.cache = cache
-exports.files = files
+exports.create = create
